c7281d149b5ea994c7c2c63676fef4362d91d2a1
pilliraghavendra <158145683+pilliraghavendra@users.noreply.github.com>
2024-02-06 13:09:18 +0530

    Update ProductDetails.cs
    chang name from hp to Lenova

--- DataAccessLayer/ProductDetails.cs
+++ DataAccessLayer/ProductDetails.cs
@@ -12,7 +12,7 @@ namespace DataAccessLayer
 
         public List<Product> GetListOfProducts()
         {
-            product.Add(new Product() { Id=1, Name="Hp", Description="Laptop", Prize=50000 });
+            product.Add(new Product() { Id=1, Name="Lenova", Description="Laptop", Prize=50000 });
             product.Add(new Product() { Id = 2, Name = "Dell", Description = "Laptop", Prize = 30000 });
             product.Add(new Product() { Id = 3, Name = "MSI", Description = "Laptop", Prize = 40000 });
             product.Add(new Product() { Id = 4, Name = "Asus", Description = "Laptop", Prize = 80000 });
